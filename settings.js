9c2c3e0fe77d4e47b6ac6c0a53e1e43b28121ac4
Balor-cmd <prinzdanny2036@gmail.com>
2025-10-07 13:15:42 +0100

    Update settings.js for Mzeusu

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~413a44ee7ed19f8b93511c159c4a4c16f3100b46",
+  OWNER_NUMBER: "+2347041521552", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
